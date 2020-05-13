148628c2f9abc3e6f8c18a372ad0e19184b4554b
danieljpietz <danieljpietz@gmail.com>
2020-05-13 15:28:47 -0700

    Initial Commit

---
+++ ORCAPriv.hpp
@@ -0,0 +1,13 @@
+//
+//  ORCAPriv.hpp
+//  ORCA
+//
+//  Created by Daniel Pietz on 5/6/20.
+//  Copyright © 2020 Daniel Pietz. All rights reserved.
+//
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+
+#pragma GCC visibility pop
